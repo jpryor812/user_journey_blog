c6e9713ac90bdace59feed67258fa169cdcdfb26
jpryor812 <jpryor812@gmail.com>
2025-01-16 17:09:03 -0500

    style fixes but change everything to Tailwind tomorrow

--- app/notification-bank/page.tsx
+++ app/notification-bank/page.tsx
@@ -35,12 +35,11 @@ export default function NotificationBank() {
       <h6 style={{ 
         fontSize: '1.1rem',
         color: '#666',
-        marginBottom: '4rem',
+        margin: '0 auto 4rem auto', 
         textAlign: 'center',
         fontWeight: '600',
         fontFamily: 'Raleway, sans-serif',
         maxWidth: '80vw',
-        margin: '0 auto'
       }}>
 Here is a catalog of all of the notifications I have collected from various popular apps like Calm, Strava, and Duolingo to inspire you to perfect your own notifications. Pay attention to the wording, intention, and tone of each notification to see how they fit the brand of the app and why they were sent by the team. 
       </h6>
@@ -76,12 +75,11 @@ Here is a catalog of all of the notifications I have collected from various popu
 <h6 style={{ 
         fontSize: '1.1rem',
         color: '#666',
-        marginBottom: '4rem',
+        margin: '0 auto 4rem auto', 
         textAlign: 'center',
         fontWeight: '600',
         fontFamily: 'Raleway, sans-serif',
         maxWidth: '80vw',
-        margin: '0 auto'
       }}>
 A pretty standard flash-sale notification to try to get a user in the app to jump on a premium offer. Classic scarcity tactic, and I wonder how effective these notifcations are at getting a user to sign up then and there for a premium subscription. If anyone has data, I would love to know!      </h6>
     <h3 className="notification-type">Notifications about reccomended content</h3>
@@ -106,12 +104,11 @@ A pretty standard flash-sale notification to try to get a user in the app to jum
 <h6 style={{ 
         fontSize: '1.1rem',
         color: '#666',
-        marginBottom: '4rem',
+        margin: '0 auto 4rem auto', 
         textAlign: 'center',
         fontWeight: '600',
         fontFamily: 'Raleway, sans-serif',
         maxWidth: '80vw',
-        margin: '0 auto'
       }}>
 These are likely their most effective notifications, especially as they get to know the user better. Both of these seem to be begginer-style content, which as a new user, is a great way to guide me into how to get the most out of Calm. I said on the app how certain begginer-like content was hidden in the vast content library, and that having them in a more accessible place would be great for new users. These notifications are in a way serving as that role of helping new users find great content to begin with. Also, showcasing a 7-day program is a great way to lock in a DAU. These are both very strong notifications to get me into the app right now. 
 </h6>
@@ -128,12 +125,11 @@ These are likely their most effective notifications, especially as they get to k
 <h6 style={{ 
         fontSize: '1.1rem',
         color: '#666',
-        marginBottom: '4rem',
+        margin: '0 auto 4rem auto', 
         textAlign: 'center',
         fontWeight: '600',
         fontFamily: 'Raleway, sans-serif',
         maxWidth: '80vw',
-        margin: '0 auto'
       }}>
 If you watched my YouTube review, these are the notifications that come as a result of me asking Calm to remind me to check in to use the app, of which I requested to be sent at 10:00pm, before my hypothetical bedtime. Nothing too special here, but they sent this same notification every day, and I wonder if they could do more. Maybe suggest a specific piece of sleep or relaxation content for me to try. Now, I had not really used the app much, so it would be hard for them to make accurate suggestions, but I got this same notification daily, and I think it could be a little more unique and dynamic, and therefore enticing to click on.</h6>
 
@@ -168,12 +164,11 @@ If you watched my YouTube review, these are the notifications that come as a res
 <h6 style={{ 
         fontSize: '1.1rem',
         color: '#666',
-        marginBottom: '4rem',
+        margin: '0 auto 4rem auto', 
         textAlign: 'center',
         fontWeight: '600',
         fontFamily: 'Raleway, sans-serif',
         maxWidth: '80vw',
-        margin: '0 auto'
       }}>
 I thought this was a really unique notification strategy, and one I appreciated. These notifications are not necessarily trying to get me to go into Calm right now, but, at least in my interpretation, made me feel like Calm is my trusted anxiety-relieving influencer. By sending these simple, yet calming notifications, I am reminded that Calm really does make me feel better, and how I should continue to use the app. I think this is a unique and likely effective strategy.</h6>
     
@@ -216,12 +211,11 @@ I thought this was a really unique notification strategy, and one I appreciated.
 <h6 style={{ 
         fontSize: '1.1rem',
         color: '#666',
-        marginBottom: '4rem',
+        margin: '0 auto 4rem auto', 
         textAlign: 'center',
         fontWeight: '600',
         fontFamily: 'Raleway, sans-serif',
         maxWidth: '80vw',
-        margin: '0 auto'
       }}>
 First, it&apos;s odd how few notifications I have received from Strava. It has been about one per week, which is very infrequent. I like the Day 1 notification a lot, including the use of the emoji, as it encourages the user to get started and feel the value proposition. The other two frankly aren&apos;t very good. I would learn more into the fitness influener pal like Calm did and send more Monday motivation like messages, but making them better. Something like, even a 30-minute walk once per day can help you lost 10 lbs. in a year. More actionable fitness data like that would be far more engaging. I also expected notifications regarding my challenges such as only two weeks left to earn a free month at Planet Fitness! Let&apos;s start that workout! I just feel like they are severely underutilizing their notifications.
 </h6>    
